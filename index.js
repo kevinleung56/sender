6b9b74e54fc89ec1927badb6ef40029625f8c350
kevinleung56 <kevinleung56@gmail.com>
2021-01-10 16:34:28 +1100

    Refactor page

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+let numUsers = 0;
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
+  let addedUser = false;
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+  socket.on('new message', (data) => {
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data,
+    });
+  });
+
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers,
+    });
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers,
+    });
+  });
+
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username,
+    });
+  });
+
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username,
+    });
+  });
+
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers,
+      });
+    }
+  });
 });
